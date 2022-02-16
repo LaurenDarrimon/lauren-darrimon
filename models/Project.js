1595ec58a88e4741613eb9989800e154785cde00
LaurenDarrimon <laurendarrimon@gmail.com>
2022-02-15 20:31:16 -0800

    create projects for portfolio projects

---
+++ models/Project.js
@@ -0,0 +1,44 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Project extends Model {}
+
+Project.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        repo_url: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        deployed_url: {
+            type: DataTypes.INTEGER,  
+        },
+        image_path: { 
+            type: DataTypes.STRING,
+            allowNull: false,
+        }
+
+    },
+    {
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'project',
+    }
+);
+
+module.exports = Project;
