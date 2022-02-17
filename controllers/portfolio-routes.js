506f571637572fb9c94d169b071cffdc76d15a86
LaurenDarrimon <laurendarrimon@gmail.com>
2022-02-16 21:57:59 -0800

    UI styling and content adding

--- controllers/portfolio-routes.js
+++ controllers/portfolio-routes.js
@@ -1,22 +1,5 @@
 const router = require('express').Router();
 const { Project } = require('../models');
-
-
-//SHOW FULL PORTFOLIO
-router.get('/', async (req, res) => {
-    try {
-        // Get all projects
-        const projectData = await Project.findAll();
-
-        const projects = projectData.map((project) => project.get({ plain: true }));
-        console.log(projects);
-
-        // Pass data to handlbars to render 
-        res.render('location', { items, location, logged_in: req.session.logged_in });
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
   
   //ONE PROJECT
   router.get('/item/:id', async (req, res) => {
