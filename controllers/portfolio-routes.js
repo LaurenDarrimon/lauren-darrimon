862e1c42f3a513fc503da4fc0fdff278b6722c6e
LaurenDarrimon <laurendarrimon@gmail.com>
2022-02-16 23:35:53 -0800

    update GET route for one portfoli project

--- controllers/portfolio-routes.js
+++ controllers/portfolio-routes.js
@@ -2,23 +2,21 @@ const router = require('express').Router();
 const { Project } = require('../models');
   
   //ONE PROJECT
-  router.get('/item/:id', async (req, res) => {
+  router.get('/:id', async (req, res) => {
     try {
-      const itemData = await Item.findByPk(req.params.id, {
+      const projectData = await Project.findByPk(req.params.id, {
         include: [{ all: true, nested: true }],
       });
   
-      const item = itemData.get({ plain: true });
+      const project = projectData.get({ plain: true });
   
       console.log("__________________________________");
-      console.log('item:');
-      console.log(item);
+      console.log('project:');
+      console.log(project);
       console.log("__________________________________");
   
-      res.render('item', { 
-        item,
-        user_id: req.session.user_id,
-        logged_in: req.session.logged_in, });
+      res.render('one-project', { 
+        project,});
     } catch (err) {
       res.status(500).json(err);
     }
