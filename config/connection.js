717d5d06472cddbeae52bf2ba59e5bbc6450fb30
LaurenDarrimon <laurendarrimon@gmail.com>
2022-02-16 23:32:51 -0800

    modify handlebars layout for portfolio, thumbnails, projects

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,5 @@
-const Sequelize = require('sequelize');
-require('dotenv').config();
+const Sequelize = require("sequelize");
+require("dotenv").config();
 
 let sequelize;
 
@@ -8,15 +8,15 @@ let sequelize;
 // sequelize = new Sequelize(process.env.JAWSDB_URL);
 // } else {
 sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
-    }
-  );
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: "localhost",
+    dialect: "mysql",
+    port: 3306,
+  }
+);
 // }
 
 module.exports = sequelize;
